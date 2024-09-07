8fe11dca7cccbaa2bc75cc982ae34a3c033fe8fd
unknown <sweetcode.js@gmail.com>
2024-09-07 23:32:33 +0600

    object portion is done

---
+++ object.js
@@ -0,0 +1,71 @@
+/* Task-1
+Access the golden rod color value in output. */
+
+/* const colors = {
+  red: "#ff0000",
+  green: "#00ff00",
+  blue: "#0000ff",
+  "golden rod": "#daa520",
+};
+
+let key = Object.keys(colors);
+let value = Object.values(colors);
+
+console.log(colors['golden rod']); */
+
+/* Task-2
+For this object below add a property named passenger capacity with value 5 */
+
+/* const car = {
+  make: "Toyota",
+  model: "Corolla",
+  year: 2020,
+};
+
+car.capacity = 5;
+
+console.log(car); */
+
+/* Task-3
+Display the physics marks as output. */
+
+/* const student = {
+  name: "Hamim Sakep",
+  id: 5421,
+  physics: {
+    subject: "HSC Physics",
+    author: "Shahjahan Tapan",
+    marks: 30,
+  },
+};
+
+let marks = student.physics.marks;
+console.log(marks); */
+
+/* 
+Task-4
+Count the number of properties. */
+
+/* let student = {
+  name: "Ariana Grande",
+  age: 21,
+  city: "Gaibandha",
+  isStudent: true,
+};
+
+console.log(Object.keys(student).length); */
+
+/* Task-5 (Hard)
+Loop through an object and print the key-value pairs with their types */
+
+
+/* let myObject = {
+  name: "John Doe",
+  age: 25,
+  city: "Example City",
+  isStudent: true,
+};
+
+for (let el in myObject) {
+  console.log(el, typeof myObject[el]);
+} */
